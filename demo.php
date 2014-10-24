7102e7437fe223f9899d979f3b99ec2b8538b117
Moon <muziyue@lost9.com>
2014-10-24 15:03:41 +0800

    完成头尾标签

---
+++ demo.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Moon
+ * Date: 14/10/24
+ * Time: 下午2:20
+ */ 
---
+++ without.class.php
@@ -0,0 +1,9 @@
+<?php
+
+class without
+{
+    function header()
+    {
+        echo '<!DOCTYPE html>';
+    }
+}
