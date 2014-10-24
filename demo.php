6069e3546d1d9057a644f9c2e2b78987995c8df8
Moon <muziyue@lost9.com>
2014-10-24 16:17:38 +0800

    初始化

--- demo.php
+++ demo.php
@@ -4,4 +4,18 @@
  * User: Moon
  * Date: 14/10/24
  * Time: 下午2:20
- */ 
+ */
+include 'without.class.php';
+
+$html = new without();
+$css = [
+    "http://cdn.bootcss.com/bootstrap/3.2.0/css/bootstrap.min.css",
+    "http://cdn.bootcss.com/bootstrap/3.2.0/css/bootstrap-theme.min.css"
+];
+$js = [
+    "http://cdn.bootcss.com/jquery/1.11.1/jquery.min.js",
+    "http://cdn.bootcss.com/bootstrap/3.2.0/js/bootstrap.min.js"
+];
+
+$html->header('without', $css);
+$html->footer($js);
